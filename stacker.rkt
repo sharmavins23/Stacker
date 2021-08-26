5e6eb37b2d6a3c13dd80e97059e50bf082ae5e2c
Vins <sharmavins23@gmail.com>
2021-08-25 22:25:49 -0400

    added lucy module for 42 expression

--- stacker.rkt
+++ stacker.rkt
@@ -1,2 +1,18 @@
 #lang br/quicklang
-42
+
+;; Define func read-syntax with input params `path` and `port`
+(define (read-syntax path port)
+  
+  ;; Define anonymous func
+  ;;  - Reads `port` to list of strings `port->lines`
+  ;;  - Stores list of strings to `src-lines`
+  (define src-lines (port->lines port))
+
+  ;; Returns code describing a module (expression)
+  ;;  - Module name is lucy
+  ;;  - Uses expander from `br` language (expressions)
+  ;;  - Evaluates the expression `42`
+  (datum->syntax #f '(module lucy br
+                       42)))
+;; Makes the read-syntax func publicly available
+(provide read-syntax)
