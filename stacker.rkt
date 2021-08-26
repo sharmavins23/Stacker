e3b5aab4802d90251e60b556790377dc76205351
Vins <sharmavins23@gmail.com>
2021-08-25 23:04:46 -0400

    Finished expander code

--- stacker.rkt
+++ stacker.rkt
@@ -1,5 +1,7 @@
 #lang br/quicklang
 
+;; ===== READER CODE =================================================================================
+
 ;; Define func read-syntax with input params `path` and `port`
 ;; NOTE: Path is not used for the project
 (define (read-syntax path port)
@@ -17,7 +19,7 @@
   ;; Inserts new forms into a return as the syntax object
   ;; - Uses a quasiquote (`) operator for string interpolation
   ;; - Uses an unquote-splicing (,@) operator for splicing in merged list
-  (define module-datum `(module stacker-mod br
+  (define module-datum `(module stacker-mod "stacker.rkt"
                           ,@src-datums))
 
   ;; Converts datum to syntax object and returns
@@ -26,3 +28,63 @@
 
 ;; Makes the read-syntax func publicly available
 (provide read-syntax)
+
+;; ===== EXPANDER CODE ===============================================================================
+
+;; Define expander macro stacker-module-begin
+;; - Takes in the pattern variable HANDLE-EXPR
+(define-macro (stacker-module-begin HANDLE-EXPR ...)
+
+  ;; Defines return syntax object
+  ;; - #' captures the current lexical context (available variables)
+  #'(#%module-begin
+     ;; Handle all expressions on the stack
+     HANDLE-EXPR ...
+     ;; Display the first value of the stack, after all handling
+     (display (first stack))))
+
+;; Makes the macro syntax publicly available outside
+(provide (rename-out [stacker-module-begin #%module-begin]))
+
+;; Creates an empty list stack
+(define stack empty)
+
+;; Defines a pop func to remove the top argument
+(define (pop-stack!)
+
+  ;; Gets the first of the stack, assigning to arg
+  (define arg (first stack))
+
+  ;; Set the stack equal to the rest of the arguments
+  (set! stack (rest stack))
+
+  ;; Returns arg
+  arg)
+
+;; Defines a push func to add a new argument
+(define (push-stack! arg)
+
+  ;; Sets the stack to a construction of the arg and the stack
+  (set! stack (cons arg stack)))
+
+;; Defines our handler function
+(define (handle [arg #f])
+
+  ;; Introduce a conditional expression with two branches
+  (cond
+    
+    ;; Branch 1 - If the argument is a number, push it to the stack
+    [(number? arg) (push-stack! arg)]
+
+    ;; Branch 2 - If the argument is addition or multiplication, pop the stack twice and calculate
+    [(or (equal? + arg) (equal? * arg))
+     ;; Pop the stack twice and concatenate into postfix notation
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     ;; Push this expression back to the stack
+     (push-stack! op-result)]))
+
+;; Makes the handler function publicly available
+(provide handle)
+
+;; Provide bindings for + and * operations from br/quicklang
+(provide + *)
