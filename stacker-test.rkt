833d0a521afc7a34d7bcc9453aa12871f211c712
Vins <sharmavins23@gmail.com>
2021-08-27 16:22:08 -0400

    modified test case

--- stacker-test.rkt
+++ stacker-test.rkt
@@ -1,15 +1,19 @@
 ;; Runs our postfix calculation stack
 ;; Current expression:
-;; * (26 / 5) + 336 * (17 - 7)
+;; * (25 * 57) / (38 * 47) * 248 + (11 - 2)
 ;; Expected outcome:
-;; * 3365.2 == 16826/5
+;; * 206.872340426
 #lang reader "stacker.rkt"
-26
-5
-/
-17
-7
--
-336
+25
+57
 *
+38
+47
+*
+/
+248
+*
+11
+2
+-
 +
